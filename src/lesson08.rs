4e868eb70b0a30c548ff07979a2fc6ae66ec1fa3
Mustafa Gedik <mstf.gdik19@gmail.com>
2024-01-03 20:20:40 +0000

    if-else if-else

---
+++ src/lesson08.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let number = -2;
+
+    if number > 0 {
+        println!("{} is positive", number);
+    } else if number < 0 {
+        println!("{} is negative", number);
+    } else {
+        println!("{} is equal to 0", number);
+    }
+}
