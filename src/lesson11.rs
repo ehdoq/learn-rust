7da079b34af12adf7832340bbd31c5505db8b079
Mustafa Gedik <mstf.gdik19@gmail.com>
2024-01-03 20:21:32 +0000

    for loop

---
+++ src/lesson11.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // usage of for loop
+    for i in 1..6 {
+        println!("{}", i);
+    }
+
+    let mut sum = 0;
+
+    // for loop to iterate over first 10 natural numbers
+    for i in 1..11 {
+        sum += i;
+    }
+
+    println!("Sum: {}", sum);
+}
